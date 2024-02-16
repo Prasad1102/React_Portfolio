c983ae0426ec0916a9fda931a6ee96e427edf149
Prasad Shelke <shelkeprasad1102@gmail.com>
2024-02-16 13:00:22 +0530

    Education Fixed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
             className="navbar__button"
             onClick={() => scrollToSection("education")}
           >
-            Education.
+            Education
           </button>
         </li>
         <li className="navbar__item">
